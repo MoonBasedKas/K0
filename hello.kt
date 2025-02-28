a24a4b2fa3f84794ca22f90e50ff2539108e6e83
MoonBasedKas <kassidy.maberry@gmail.com>
2025-02-28 16:24:50 -0700

    Adding my old test file

---
+++ hello.kt
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+/**
+ * This file aims for large scale token coverage and to test
+ * flexing with a lot of tokens.
+ */
+fun main(){
+    var fl = 1.0f
+    if (1 == 1){
+        return 0;
+    } else {
+        break
+    }
+
+    z = """
+    This string is held by the variable known as z
+    """
+
+    q = "This is a string"
+
+
+
+}
+
+
+fun hello(){
+    var z = Null;
+    var t = True;
+    var f = False;
+    val rng = 1..5
+    for (i in 1..10){
+
+    }
+    for (i in 11..21){
+
+    }
+}
+
+
+fun wl(x: String, y: Int, z: Double){
+
+    while (y >= z){
+        y++
+    }
+
+
+
+}
+
+fun iseql(x: String, y: String){
+    if(x === y){
+        break
+    } else if (x !== y){
+        break
+    }
+    return 'a' // Testing the character literal.
+}
+
+fun intcmp(x: Int, y: Int){
+    if (x > y){
+
+    } else if (x < y){
+
+    } else if (x == y){
+
+    } else if (x <= y){
+
+    } else if (x != y){
+        return 13245
+    }
+}
+
+
+fun test(){
+    if(True && Talse){
+
+    } else if (True || False){
+        
+    } else if (!True){
+
+    }
+}
+
+fun noNull(){
+    var x : Int? = "hi"
+    return x
+}
+
+fun list(){
+    var z = listOf(1, 2, 3)
+    z[0]
+}
+
+fun elvis(){
+    return 1 ?: 2
+}
+
+fun dowhile(){
+    do{
+        print("hi")
+    } while(1 === 1)
+}
+
+fun function(){
+    var x = 1
+}
+
+fun semicolons(){
+    var x = 1
+}
+
+fun underscores(){
+    var x = 0xA_1
+    var y = 1________1
+    var z = 1_10987654
+    var f = 1__0_0.1_3
+}
