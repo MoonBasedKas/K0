65a2d894b5b033709442b6bcbd641b5db6f4068e
MoonBasedKas <kassidy.maberry@gmail.com>
2025-03-05 15:05:12 -0700

    Wrote more of the symtab stuff

--- symTab.h
+++ symTab.h
@@ -1,11 +1,15 @@
 
 
 struct symTab{
-
+    char *s; // uhhh nice?
+    struct symTab *parent;
+    struct SymEntry **buckets;
 };
 
 struct symEntry{
-
+    char *name;
+    void *type;
+    struct symTab *parent;  
 };
 
 
@@ -14,6 +18,6 @@ struct symTab *root;
 int add(char *elem, void *type);
 struct symEntry *contains(char *elem);
 
-
+struct symTab *createTable(); // call calloc 
 int hash(char *elem);
 int check(char *elem);
