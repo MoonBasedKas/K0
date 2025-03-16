289690ef4678c38096d2a608f2f5d82b07c496d6
MoonBasedKas <kassidy.maberry@gmail.com>
2025-03-15 18:46:19 -0600

    Modified test file to be more extreme

--- TestCases/test3.kt
+++ TestCases/test3.kt
@@ -4,18 +4,30 @@ fun f() : Int {
 }
 
 fun g() : Int {
+   var z : Int = 23
    return 2*2
 }
 
 fun y() : Int {
+   var qq : Int = 23
    return 2*2
 }
 
 fun q() : Int {
+
+   var adsf : Int = 23
+
+   var zasd: Int = 23
+   var zd : Int = 23
+   var zf : Int = 23
    return 2*2
 }
 
 fun w() : Int {
+    fun q(): Int{
+        var x : String = 2
+        var e : Double = 2.6
+    }
    return 2*2
 }
 
