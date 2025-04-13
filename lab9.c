d87735ae802144a7d697c622910ca61ff45652bc
Erik Noll <ErikNoll78@Gmail.com>
2025-04-13 16:28:36 -0600

    handle some globals

--- lab9.c
+++ lab9.c
@@ -8,10 +8,48 @@ int main(int argc, char *argv[])
 
     //fake symbol table
     struct addr *addrI = genLocal(8);
+    //.string 8
+    {
+        struct addr *global8 = malloc(sizeof(struct addr));
+        global8->region = R_GLOBAL;
+        global8->u.offset = 8;
 
+        instrList = gen(D_GLOB, global8, NULL, NULL);
+        nextInstr = instrList;
+    }
+
+    //"Variable i is %d.\000"
+    {
+        struct addr *strVar = malloc(sizeof(struct addr));
+        strVar->region = R_NAME;
+        strVar->u.name = "Variable i is %d.\000";
+
+        struct instr *i = gen(D_GLOB, strVar, NULL, NULL);
+        nextInstr->next = i;
+        nextInstr = nextInstr->next;
+    }
+
+    // proc main,0,32
+    {
+        struct addr *procName = malloc(sizeof(struct addr));
+        procName->region = R_NAME;
+        procName->u.name = "main";
+
+        struct addr *procParam = malloc(sizeof(struct addr));
+        procParam->region = R_CONST;
+        procParam->u.offset = 0;
+
+        struct addr *procOffset = malloc(sizeof(struct addr));
+        procOffset->region = R_CONST;
+        procOffset->u.offset = 32;
+
+        struct instr *procInstr = gen(D_PROC, procName, procParam, procOffset);
+        nextInstr->next = procInstr;
+        nextInstr = nextInstr->next;
+    }
     //i = 5
-    instrList = gen(O_ASN, addrI, NULL, NULL); //2nd arg needs to be a const
-    nextInstr = instrList;
+    nextInstr->next = gen(O_ASN, addrI, NULL, NULL); //2nd arg needs to be a const
+    nextInstr = nextInstr->next;
 
     //i * i
     nextInstr->next = gen(O_MUL, genLocal(8), addrI, addrI);
