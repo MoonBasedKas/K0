af459cd3c43a7470cbd459d307f40af75f791f3b
MoonBasedKas <156983695+MoonBasedKas@users.noreply.github.com>
2025-04-18 10:21:41 -0600

    Create evil_if_test.kt

---
+++ TestCasesOld/evil_if_test.kt
@@ -0,0 +1,121 @@
+fun main(): Int {
+    return 0;
+}
+
+fun stdIf() : Int {
+    if (1 == 1) {
+        return 1;
+    }
+
+    if (1 == 1)
+    {
+        return 1;
+    }
+
+}
+
+fun ifelse() : Int {
+    if (1 == 1)
+    {
+        return 1;
+    } else {
+        return 1;
+    }
+
+
+    if (1 == 1)
+    {
+        return 1;
+    } 
+    else {
+        return 1;
+    }
+
+
+    if (1 == 1){
+        return 1;
+    } else 
+    {
+        return 1;
+    }
+
+    if (1 == 1)
+    {
+        return 1;
+    } else 
+    {
+        return 1;
+    }
+
+    if (1 == 1)
+    {
+        return 1;
+    } 
+    else 
+    {
+        return 1;
+    }
+}
+
+
+fun elif(){
+    if (1 == 1){
+        return 1;
+    } else if (1==1){
+        return 1;
+    } else {
+        return 1;
+    }
+
+
+    if (1 == 1){
+        return 1;
+    } else if (1==1){
+        return 1;
+    } 
+    else {
+        return 1;
+    }
+
+
+
+    if (1 == 1){
+        return 1;
+    } else if (1==1)
+    {
+        return 1;
+    } else {
+        return 1;
+    }
+
+
+    if (1 == 1){
+        return 1;
+    } else if (1==1)
+    {
+        return 1;
+    } else 
+    {
+        return 1;
+    }
+
+    if (1 == 1)
+    {
+        return 1;
+    } else if (1==1)
+    {
+        return 1;
+    } else {
+        return 1;
+    }
+
+
+    if (1 == 1){
+        return 1;
+    } else if (1==1){
+        return 1;
+    } else 
+    {
+        return 1;
+    }
+}
