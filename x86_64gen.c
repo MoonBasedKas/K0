10cc15dd10f75189439e664337534083a1704b50
MoonBasedKas <kassidy.maberry@gmail.com>
2025-05-08 15:56:57 -0600

    Updated swear words

--- x86_64gen.c
+++ x86_64gen.c
@@ -697,7 +697,7 @@ int translateIcToAsm(struct tree *root)
                 emit("\tmovq\t%s, %%rax", regs[r_s1_dividend].name); // Dividend to %rax
                 emit("\tcqto");                                     // Sign-extend %rax into %rdx:%rax for idivq
                 emit("\tidivq\t%s", regs[r_s2_divisor].name);      // Divide %rdx:%rax by divisor reg
-                // holy fuck
+                // Praise by the Sun
 
                 freeGPR(r_s1_dividend);
                 freeGPR(r_s2_divisor);
