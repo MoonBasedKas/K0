46eadcfc262d82bc4387a2533e52e691a9b457b9
Erik Noll <ErikNoll78@Gmail.com>
2025-05-07 23:52:40 -0600

    fuck

--- x86_64gen.c
+++ x86_64gen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "x86_64gen.h"
 #include "tac.h"
 #include "tree.h"
@@ -34,45 +35,53 @@ static char **asmBuf;
 static int bufCap = 0;
 static int bufLen = 0;
 
+/**
+ *  @brief Initialize assembly buffer
+ */
 static void initAsmBuf(void) {
     bufCap = 128;
     bufLen = 0;
     asmBuf = malloc(bufCap * sizeof(*asmBuf));
-    if (!asmBuf){
-        fprintf(stderr, "Failed to allocate memory for asmBuf\n");
+    if (!asmBuf) {
+        fprintf(stderr, "Failed to allocate memory for asmBuf()\n");
         exit(1);
     }
 }
 
-static void pumpUpTheJam(const char *fmt, ...) {
+/**
+ *  @brief Emit assembly
+ *  @param fmt Format string
+ *  @param ... Arguments va_list goated
+ */
+static void emit(const char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    char tmp[256];
-    vsnprintf(tmp, sizeof(tmp), fmt, ap);
+    char line[256];
+    vsnprintf(line, sizeof(line), fmt, ap);
     va_end(ap);
 
     if (bufLen >= bufCap) {
         bufCap *= 2;
         asmBuf = realloc(asmBuf, bufCap * sizeof(*asmBuf));
         if (!asmBuf){
-            fprintf(stderr, "Can't pump up the jam :(\n");
+            fprintf(stderr, "Out of memory in emit()\n");
             exit(1);
         }
     }
-    asmBuf[bufLen++] = strdup(tmp);
+    asmBuf[bufLen++] = strdup(line);
 }
 
 /**
  *  @brief Initialize register pools
  */
-void initRegs(void){
-    for (int i = 0; i < NUM_GPR; i++){
+void initRegs(void) {
+    for (int i = 0; i < NUM_GPR; i++) {
         strncpy(regs[i].name, gprNames[i], sizeof(regs[i].name));
         regs[i].name[sizeof(regs[i].name) - 1] = '\0';
         regs[i].status = 0;
         regs[i].a = NULL;
     }
-    for (int i = 0; i < NUM_XMM; i++){
+    for (int i = 0; i < NUM_XMM; i++) {
         strncpy(xmmRegs[i].name, xmmNames[i], sizeof(xmmRegs[i].name));
         xmmRegs[i].name[sizeof(xmmRegs[i].name) - 1] = '\0';
         xmmRegs[i].status = 0;
@@ -84,7 +93,6 @@ void initRegs(void){
  *  @brief Initialize address map
  */
 void initAddrDescrip(void){
-    // TODO
     addrMap = NULL;
 }
 
@@ -93,8 +101,7 @@ void initAddrDescrip(void){
  *  @param a Address to get descriptor for
  *  @return Address descriptor
  */
-struct addr_descrip *getAddrDesc(struct addr *a){
-    // Create new for now, need lookup later
+struct addr_descrip *getAddrDesc(struct addr *a) {
     struct addr_descrip *d = malloc(sizeof(*d));
     if (!d){
         fprintf(stderr, "Failed to allocate memory for addr_descrip\n");
@@ -103,17 +110,35 @@ struct addr_descrip *getAddrDesc(struct addr *a){
     d->status = 1;
     d->r = NULL;
     d->a = *a;
-    // Add to list dumbass
     d->next = addrMap;
     addrMap = d;
     return d;
 }
 
 /**
+ *  @brief Emit data section
+ *  @param head Head of TAC list
+ */
+static void emitDataSection(struct instr *head) {
+    bool dataStarted = false;
+    for (struct instr *p = head; p; p = p->next) {
+        if (p->opcode == D_GLOB && p->dest && p->dest->region == R_GLOBAL) {
+            if (!dataStarted) {
+                emit(".data");
+                dataStarted = true;
+            }
+            // use our lab 11 stuff
+            emit(".globl %s", p->dest->u.name);
+            emit("%s:", p->dest->u.name);
+            emit("\t.long %d", p->src1->u.offset);
+        }
+    }
+}
+/**
  *  @brief Get free GPR
  *  @return Index of free GPR
  */
-int getGPR(void){
+int getGPR(void) {
     for (int i = 0; i < NUM_GPR; i++){
         if (regs[i].status == 0){
             regs[i].status = 1;
@@ -128,7 +153,7 @@ int getGPR(void){
  *  @brief Free GPR
  *  @param idx Index of GPR to free
  */
-void freeGPR(int idx){
+void freeGPR(int idx) {
     if (idx < 0 || idx >= NUM_GPR) return;
     regs[idx].status = 0;
     regs[idx].a = NULL;
@@ -138,7 +163,7 @@ void freeGPR(int idx){
  *  @brief Get free XMM
  *  @return Index of free XMM
  */
-int getXMM(void){
+int getXMM(void) {
     for (int i = 0; i < NUM_XMM; i++){
         if (xmmRegs[i].status == 0){
             xmmRegs[i].status = 1;
@@ -153,22 +178,39 @@ int getXMM(void){
  *  @brief Free XMM
  *  @param idx Index of XMM to free
  */
-void freeXMM(int idx){
+void freeXMM(int idx) {
     if (idx < 0 || idx >= NUM_XMM) return;
     xmmRegs[idx].status = 0;
     xmmRegs[idx].a = NULL;
 }
 
 /**
- *  @brief Ensure address is in GPR
- *  @param a Address to ensure in GPR
+ *  @brief Ensure IR address is in GPR
+ *  @param a Address to ensure in GPR (IR ptr)
  *  @param outReg Output register index
- *  @return 1 if address is in GPR, 0 otherwise
+ *  @return 0 success
  */
-int ensureInGPR(struct addr *a, int *outReg){
+int ensureInGPR(struct addr *a, int *outReg) {
     int r = getGPR();
     regs[r].a = a;
-    // TODO: Need to insert movq to load from memory to regs[r].name I think
+    switch (a->region){
+        case R_LOCAL:
+            emit("\tmovq\t-%d(%%rbp), %s", a->u.offset, regs[r].name);
+            break;
+        case R_GLOBAL:
+            emit("\tmovq\t%s(%%rip), %s", a->u.name, regs[r].name);
+            break;
+        case R_CONST:
+            emit("\tmovq\t$%d, %s", a->u.offset, regs[r].name);
+            break;
+        case R_LABEL:
+            emit("\tleaq\tL%d(%%rip), %s", a->u.offset, regs[r].name);
+            break;
+        default:
+            emit("\t# unknown region %d in ensureInGPR()", a->region);
+            break;
+        // Need String and uh name?
+    }
     *outReg = r;
     return 0;
 }
@@ -179,37 +221,140 @@ int ensureInGPR(struct addr *a, int *outReg){
  *  @param outReg Output register index
  *  @return 1 if address is in XMM, 0 otherwise
  */
-int ensureInXMM(struct addr *a, int *outReg){
+int ensureInXMM(struct addr *a, int *outReg) {
     int r = getXMM();
     xmmRegs[r].a = a;
-    // TODO: Same as above
+    switch (a->region){
+        case R_LOCAL:
+            emit("\tmovq\t-%d(%%rbp), %s", a->u.offset, xmmRegs[r].name);
+            break;
+        case R_GLOBAL:
+            emit("\tmovq\t%s(%%rip), %s", a->u.name, xmmRegs[r].name);
+            break;
+        default:
+            emit("\t# unknown region %d in ensureInXMM()", a->region);
+            break;
+    }
     *outReg = r;
     return 0;
 }
 
 /**
- *  @brief Spill address to memory
- *  @param a Address to spill
+ *  @brief Spill a GPR-backed IR addr back to memory
+ *  I'm pretty sure this is just a movq to the stack
+ *  based on the region and then we dump the register
+ *  @param a IR Address ptr to spill
  */
-void spillAddr(struct addr *a){
-    // TODO idk how to do this yet
+void spillAddr(struct addr *a) {
+    for  (struct addr_descrip *d = addrMap; d; d = d->next) {
+        if (d->a.region == a->region && d->a.u.offset == a->u.offset && d->r) {
+            int idx = (struct regdescrip *)d->r - regs;
+            switch (a->region){
+                case R_LOCAL:
+                    emit("\tmovq\t%s, -%d(%%rbp)", regs[idx].name, a->u.offset);
+                    break;
+                case R_GLOBAL:
+                    emit("\tmovq\t%s, %s(%%rip)", regs[idx].name, a->u.name);
+                    break;
+                default:
+                    emit("\t# unknown region %d in spillAddr()", a->region);
+                    break;
+            }
+            freeGPR(idx);
+            d->r = NULL;
+            return;
+        }
+    }
+    fprintf(stderr, "Spill failed for %s\n", a->u.name);
+    exit(1);
 }
 
 /**
  *  @brief Translate IR to x86-64 assembly
  *  @return 1 if translation is successful, 0 otherwise
  */
-int translateIcToAsm(){
+int translateIcToAsm() {
+    extern struct tree *root;
+    struct instr *p;
+
     initRegs();
     initAddrDescrip();
-    // TODO walk the TAC list and pump instructions out
-    return 0;
+    initAsmBuf();
+
+    emitDataSection(root->icode);
+    emit(".text");
+
+    for (p = root->icode; p; p = p->next){
+        switch (p->opcode){
+            case D_PROC:
+                char *fname = p->dest->u.name;
+                int locals = p->src1->u.offset;
+                emit(".globl %s", fname);
+                emit("%s:", fname);
+                emit("\tpushq\t%%rbp");
+                emit("\tmovq\t%%rsp, %%rbp");
+                if (locals > 0){
+                    emit("\tsubq\t$%d, %%rsp", locals);
+                }
+                break;
+            case D_END:
+                if (p->src1->u.offset > 0)
+                    emit("\taddq\t$%d, %%rsp", p->src1->u.offset);
+                emit("\tpopq\t%%rbp");
+                emit("\tret");
+                break;
+            case D_LABEL:
+                emit("L%d:", p->dest->u.offset);
+                break;
+            case O_GOTO:
+                emit("\tjmp L%d", p->src1->u.offset);
+                break;
+            case O_BEQ: case O_BNE: case O_BLT:  case O_BLE:
+            case O_BGT: case O_BGE:
+                int r1, r2;
+                ensureInGPR(p->src1, &r1);
+                ensureInGPR(p->src2, &r2);
+                emit("\tcmpq %s, %s", regs[r2].name, regs[r1].name);
+
+                const char *mn =
+                    (p->opcode == O_BEQ) ? "je"  :
+                    (p->opcode == O_BNE) ? "jne" :
+                    (p->opcode == O_BLT) ? "jl"  :
+                    (p->opcode == O_BLE) ? "jle" :
+                    (p->opcode == O_BGT) ? "jg"  : "jge";
+                emit("\t%s\tL%d", mn, p->dest->u.offset);
+                freeGPR(r1);
+                freeGPR(r2);
+                break;
+            case O_ADD: case O_SUB: case O_MUL:
+                int rd, r1, r2;
+                ensureInGPR(p->dest, &rd);
+                ensureInGPR(p->src1, &r1);
+                ensureInGPR(p->src2, &r2);
+                const char *op =
+                    (p->opcode == O_ADD) ? "addq" :
+                    (p->opcode == O_SUB) ? "subq" : "imulq";
+                emit("\tmovq\t%s, %s", regs[r1].name, regs[rd].name);
+                emit("\t%s\t%s, %s", op, regs[r2].name, regs[rd].name);
+                spillAddr(p->dest);
+                break;
+            default:
+                fprintf(stderr, "Unknown opcode %d in translateIcToAsm()\n", p->opcode);
+                break;
+        }
+    }
+    return 1;
 }
 
 /**
  *  @brief Write x86-64 assembly to file
  */
 int writeAsm(FILE *fp){
-    // TODO: output .text and .rodata sections and buffered asm
-    return 0;
+    if (!fp) return 0;
+    for (int i = 0; i < bufLen; i++){
+        fprintf(fp, "%s\n", asmBuf[i]);
+        free(asmBuf[i]);
+    }
+    free(asmBuf);
+    return 1;
 }
